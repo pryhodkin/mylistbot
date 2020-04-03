4eafcca8e06c9a5e135ed10eec898a5491bf1cc1
Yaroslav Pryhodko <yarikmaster01022002@gmail.com>
2020-04-03 20:19:19 +0300

    All works. Now...

---
+++ classes.py
@@ -0,0 +1,121 @@
+import psycopg2
+
+
+class Database:
+    
+    def __init__(self, connection):
+        self.connection = connection
+
+    def ToSQLString(self, string):
+        if string == None:
+            return "None"
+
+        i = 0
+        result = ""
+        while i < len(string):
+            if(i == "'"):
+                result += "'"
+            result += string[i]
+            i += 1
+        return result
+
+    def AddItemToList(self, item, user):
+        item = self.ToSQLString(item)
+        cursor = self.connection.cursor()
+        cursor.execute(f"""
+                            INSERT INTO list(id, author, item)
+                            VALUES (DEFAULT, '{user}', '{item}');
+                       """)
+        self.connection.commit()
+        self.Renum()
+        cursor.close()
+
+    def GetList(self):
+        cursor = self.connection.cursor()
+        cursor.execute("""SELECT * FROM list;""")
+        result = ""
+        for item in cursor:
+            result += str(item[0]) + '. ' + str(item[2]) + '\n'
+        cursor.close()
+        return result
+
+    def DeleteFromList(self, id):
+        cursor = self.connection.cursor()
+        cursor.execute(f"""
+                        DELETE FROM list
+                        WHERE id = {id};
+                       """)
+        self.connection.commit()
+        cursor.close()
+        self.Renum()
+
+    def CreatDB(self):
+        cursor = self.connection.cursor()
+        cursor.execute("""DROP TABLE list;""")
+        cursor.execute("""
+        CREATE TABLE list
+                      (
+                        id        SERIAL          PRIMARY KEY      NOT NULL,
+                        author    VARCHAR(100)                             ,
+                        item      VARCHAR(10000)                           
+                      );
+                      """)
+        self.connection.commit()
+        cursor.close()
+
+    def Renum(self):
+        cursor = self.connection.cursor()
+        updator = self.connection.cursor()
+        cursor.execute("""
+                        SELECT id FROM list;
+                      """)
+        n = 0
+        for i in cursor:
+            n += 1
+            updator.execute(f"""UPDATE list SET id = {n}
+                               WHERE id = {i[0]}""")
+        self.connection.commit()
+        updator.close()
+        cursor.close()
+    
+def DatabaseVars(connect_string):
+    i = 0
+    connection = {
+        'user': '',
+        'password': '',
+        'host': '',
+        'port': '',
+        'database': ''
+    }
+    condition = 'None'
+
+    while i < len(connect_string):
+
+        if connect_string[i] == '/':
+            if connect_string[i - 1] == '/':
+                condition = 'user'
+            else:
+                condition = 'database'
+                
+        elif connect_string[i] == ':' and condition == 'user':
+            condition = 'password'
+            
+        elif connect_string[i] == '@' and condition == 'password':
+            condition = 'host'
+            
+        elif connect_string[i] == ':' and condition == 'host':
+            condition = 'port'
+            
+        elif condition != 'None':
+            connection[condition] += connect_string[i]
+        i += 1
+    return connection
+
+def GetDelIndex(string):
+    string = string.lower()
+    string = string.replace(" ", "")
+    string = string.replace("del", "")
+    string = str(string)
+    if string.isdecimal() == False or string.isdigit() == False:
+        return ''
+    return int(string)
---
+++ main.py
@@ -0,0 +1,65 @@
+import telebot
+import consts
+import psycopg2
+import classes
+import os
+
+def GenerateId():
+    consts.iden += 1
+    return consts.iden
+
+bot = telebot.TeleBot("1215734535:AAFj1YBR8FIDJMIp2tpIhBvqLuv_EYX15uA")
+
+connect_vars = classes.DatabaseVars(str(os.environ['DATABASE_URL']))
+
+connection = psycopg2.connect(
+    host = connect_vars['host'],
+    database = connect_vars['database'],
+    user = connect_vars['user'],
+    password = connect_vars['password']
+)
+
+db = classes.Database(connection);
+
+@bot.chosen_inline_handler(lambda result: True)
+def handle_answer(result):
+    if(result.query == 'get'):
+        pass
+    elif(result.query.startswith("del")):
+        db.DeleteFromList(classes.GetDelIndex(result.query))
+    else:
+        db.AddItemToList(result.query, result.from_user.username)
+
+
+
+@bot.inline_handler(lambda query: (query.from_user.id == 371404061 or query.from_user.id == 391960442) and query.query == 'get')
+def handle_get(query):
+    text = db.GetList()
+    if text == "":
+        text = "list is empty"
+    result = telebot.types.InlineQueryResultArticle(int(query.id) + GenerateId(), 'get list', telebot.types.InputTextMessageContent(text))
+    bot.answer_inline_query(query.id, [result], cache_time = 20)
+
+
+
+@bot.inline_handler(lambda query: (query.from_user.id == 371404061 or query.from_user.id == 391960442) and query.query.startswith("del"))
+def handle_get(query):
+    result = telebot.types.InlineQueryResultArticle(query.id, f'del No. {classes.GetDelIndex(query.query)}', telebot.types.InputTextMessageContent(f"item No.{classes.GetDelIndex(query.query)} deleted"))
+    bot.answer_inline_query(query.id, [result], cache_time = 20)
+
+
+
+@bot.inline_handler(lambda query: (query.from_user.id == 371404061 or query.from_user.id == 391960442))
+def handle_query(query):
+    result = telebot.types.InlineQueryResultArticle(query.id, "new item:" + query.query, telebot.types.InputTextMessageContent("item: " + query.query + " added"))
+    bot.answer_inline_query(query.id, [result], cache_time = 20)
+
+
+
+@bot.message_handler(func = lambda message: True)
+def handle_start(message):
+    bot.send_message(message.from_user.id, "only for inline using, start type @list_helper_bot")
+    
+
+
+bot.polling(none_stop = True, interval = 0)
